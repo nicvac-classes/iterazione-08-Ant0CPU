73d97668834dd8fd8b0e03cb0aaa6dea75c6ba76
Ant0CPU <antonio.bruno@marconibari.edu.it>
2024-10-16 15:48:07 +0000

    Consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,46 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+    int N, i, p, d;
+    float voto, somma, media;
+    string stato, nome;
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+    somma=0;
+    p=0;
+    d=0;
+
+    do 
+    {
+        cout<<"Quanti studenti ci sono in una classe?"<<endl;
+        cin>>N;
+    }
+    while (N<0);
+
+    for(i=1; i<=N; i++ )
+    {
+        cout<<"Come si chiama lo studente?"<<endl;
+        cin>>nome;
+        cout<<"Nome: "<<nome<<endl;
+        
+        cout<<"Inserire il suo voto il informatica"<<endl;
+        cin>>voto;
+        if (voto>=6)
+        {
+            stato="Promosso";
+            p=p+1;
+        }
+        else
+        {
+            stato="Ha il debito";
+            d=d+1;
+        }
+
+        cout<<stato<<endl;
+        somma=somma+voto;
+    }
+    media=somma/(i-1);
+    cout<<"Media della classe: " <<media<<endl;
+    
+    cout<<"Sono stati promossi: " <<p<<endl;
+    cout<<"Hanno avuto il debito: " <<d<<endl;
+}
